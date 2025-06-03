79f4eea86fefdddd2875842cd185055f83b97fb8
tofi05 <abdellatifkichah@gmail.com>
2025-06-03 16:50:41 +0100

    starting

--- header.php
+++ header.php
@@ -1,44 +1,111 @@
 <!DOCTYPE html>
 <html dir="rtl" lang="ar">
-
   <head>
     <meta charset="UTF-8" />
+    <link rel="icon" type="image/svg+xml" href=<?php echo get_theme_file_uri('public/faviconB&W.svg') ?> />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <?php wp_head();?>
-    <title>Dehimi Organic Store</title>
+    <?php wp_head(); ?>
+    <title>Dehimi Project</title>
   </head>
   <body>
-    <header class="header">
-      <div class=" flex">
-      <a href="#" class="logo">
-        <img class="logo" src="<?php echo get_theme_file_uri('public/img/Color = Dark.png') ?>"></img>
-      </a>
-      <button aria-expanded="false" class="icons" aria-controls="primary-navigation">
-        <span class="sr-only">Menu</span>
-        <i class="fa-solid fa-bars"></i>        
-        <i class="fa-solid fa-x hidden"></i>      
-      </button>
-      <nav aria-hidden="false" class="navbar hide" id="primary-navigation" >
-        <ul class="nav-links">
-          <li>
-            <a href="">الصفحة الرئيسة</a>
-          </li>
-          <li>
-            <a href="#">المتجر</a>
-          </li>
-          <li>
-            <a href="#">من نحن</a>
-          </li>
-          <li>
-            <a href="#">إتصل بنا</a>
-          </li>
-        </ul>
-      </nav>
-      <div class="nav-actions">
-        <a href="#" class="search">
-          <i class="fa-solid fa-magnifying-glass"></i>        </a>  
-        </a>  
-        <a href="#" class="shopping">
-          <i class="fa-solid fa-cart-shopping"></i>        </a>
+  <header class="header">
+      <div class="header-container">
+        <div class="nav-actions">
+          <a href="#" class="search">
+            <svg
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M18.319 14.4326C20.7628 11.2941 20.542 6.75347 17.6569 3.86829C14.5327 0.744098 9.46734 0.744098 6.34315 3.86829C3.21895 6.99249 3.21895 12.0578 6.34315 15.182C9.22833 18.0672 13.769 18.2879 16.9075 15.8442C16.921 15.8595 16.9351 15.8745 16.9497 15.8891L21.1924 20.1317C21.5829 20.5223 22.2161 20.5223 22.6066 20.1317C22.9971 19.7412 22.9971 19.1081 22.6066 18.7175L18.364 14.4749C18.3493 14.4603 18.3343 14.4462 18.319 14.4326ZM16.2426 5.28251C18.5858 7.62565 18.5858 11.4246 16.2426 13.7678C13.8995 16.1109 10.1005 16.1109 7.75736 13.7678C5.41421 11.4246 5.41421 7.62565 7.75736 5.28251C10.1005 2.93936 13.8995 2.93936 16.2426 5.28251Z"
+                fill="currentColor"
+              />
+            </svg>
+          </a>
+          <a href="#" class="shopping">
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M5.79166 2H1V4H4.2184L6.9872 16.6776H7V17H20V16.7519L22.1932 7.09095L22.5308 6H6.6552L6.08485 3.38852L5.79166 2ZM19.9869 8H7.092L8.62081 15H18.3978L19.9869 8Z"
+                fill="currentColor"
+              />
+              <path
+                d="M10 22C11.1046 22 12 21.1046 12 20C12 18.8954 11.1046 18 10 18C8.89543 18 8 18.8954 8 20C8 21.1046 8.89543 22 10 22Z"
+                fill="currentColor"
+              />
+              <path
+                d="M19 20C19 21.1046 18.1046 22 17 22C15.8954 22 15 21.1046 15 20C15 18.8954 15.8954 18 17 18C18.1046 18 19 18.8954 19 20Z"
+                fill="currentColor"
+              />
+            </svg>
+          </a>
+        </div>
+        <nav aria-hidden="false" class="navbar hide" id="primary-navigation">
+          <ul class="nav-links">
+            <li>
+              <a href="/product-page.html">الصفحة الرئيسة</a>
+            </li>
+            <li>
+              <a href="/categories.html">المتجر</a>
+            </li>
+            <li>
+              <a href="/pages.html">من نحن</a>
+            </li>
+            <li>
+              <a href="#">إتصل بنا</a>
+            </li>
+          </ul>
+        </nav>
+
+        <a href="#" class="logo">
+          <img class="logo" src="/imgs/Logo.svg" />
+        </a>
+        <button
+          aria-expanded="false"
+          class="icons"
+          aria-controls="primary-navigation"
+        >
+          <span class="sr-only">Menu</span>
+          <svg
+            class="nav-open"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M2 6C2 5.44772 2.44772 5 3 5H21C21.5523 5 22 5.44772 22 6C22 6.55228 21.5523 7 21 7H3C2.44772 7 2 6.55228 2 6Z"
+              fill="currentColor"
+            />
+            <path
+              d="M2 12.0322C2 11.4799 2.44772 11.0322 3 11.0322H21C21.5523 11.0322 22 11.4799 22 12.0322C22 12.5845 21.5523 13.0322 21 13.0322H3C2.44772 13.0322 2 12.5845 2 12.0322Z"
+              fill="currentColor"
+            />
+            <path
+              d="M3 17.0645C2.44772 17.0645 2 17.5122 2 18.0645C2 18.6167 2.44772 19.0645 3 19.0645H21C21.5523 19.0645 22 18.6167 22 18.0645C22 17.5122 21.5523 17.0645 21 17.0645H3Z"
+              fill="currentColor"
+            />
+          </svg>
+          <svg
+            class="nav-close hidden"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M6.2253 4.81108C5.83477 4.42056 5.20161 4.42056 4.81108 4.81108C4.42056 5.20161 4.42056 5.83477 4.81108 6.2253L10.5858 12L4.81114 17.7747C4.42062 18.1652 4.42062 18.7984 4.81114 19.1889C5.20167 19.5794 5.83483 19.5794 6.22535 19.1889L12 13.4142L17.7747 19.1889C18.1652 19.5794 18.7984 19.5794 19.1889 19.1889C19.5794 18.7984 19.5794 18.1652 19.1889 17.7747L13.4142 12L19.189 6.2253C19.5795 5.83477 19.5795 5.20161 19.189 4.81108C18.7985 4.42056 18.1653 4.42056 17.7748 4.81108L12 10.5858L6.2253 4.81108Z"
+              fill="currentColor"
+            />
+          </svg>
+        </button>
       </div>
     </header>
